ef0800cf19fd7ed826224aa3a598453f0f0ce768
ABertie <alberte.reuss@gmail.com>
2024-08-30 13:15:05 +0200

    fixed icons

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,18 +1,19 @@
 import Link from "next/link";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
 
 export default function Socials({ data = [] }) {
     return (
         <>
             {data.map(item =>
-                <Link key={item.link.uid} href={item.link.data.link.url} target={item.link.data.link.target}>
-                    {/* <FontAwesomeIcon icon={fab.faInstagram}/>  */}
-                    {/* {item.icon} */}
-                    {item.link.data.name}
+                <Link
+                    key={item.link.uid}
+                    href={item.link.data.link.url}
+                    target={item.link.data.link.target}
+                    className="flex items-center justify-center"
+                >
+                    <FontAwesomeIcon icon={item.icon} />
+                    {/* {item.link.data.name} */}
                 </Link>
             )}
         </>
